28767f5701312ea88b7530c1f43a6eaac93df2bd
zuoxue <zuoxue@qq.com>
2019-04-01 19:40:11 +0800

    save

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <curses.h> 
+   
+int main()  { 
+    initscr(); 
+    move( 5, 15 ); 
+    printw( "%s", "Hello world" ); 
+    refresh(); 
+    sleep(2); 
+    endwin(); 
+    exit(EXIT_SUCCESS);
+}
